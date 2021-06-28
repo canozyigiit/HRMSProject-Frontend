0a008d5cda4cd2bab9a490d99b60e10153a5eaa9
Casper <muhammetcanozyigit@gmail.com>
2021-06-28 20:18:31 +0300

    28-06-2021

--- src/components/JobSeekerProfile/JobSeekerSideBar.js
+++ src/components/JobSeekerProfile/JobSeekerSideBar.js
@@ -16,17 +16,14 @@ export default function JobSeekerSideBar() {
             <div className="dashbord_nav_list">
                 <ul>
                 
-                   <Link to="/jobSeeker/profil"> <li><a ><span className="flaticon-profile"></span> Profile</a></li></Link>
-                    <Link to="/jobSeeker/resume"><li className="active"><a ><span className="flaticon-resume"></span> Resume</a></li></Link>
-                    <li><a ><span className="flaticon-paper-plane"></span> Applied Jobs</a></li>
+                   <Link to="/jobSeeker/profil"> <li><a ><span className="flaticon-profile"></span> Profil</a></li></Link>
+                    <Link to="/jobSeeker/resume"><li className="active"><a ><span className="flaticon-resume"></span> Cv</a></li></Link>
+                    <li><a ><span className="flaticon-paper-plane"></span> Başvurulan İlanlar</a></li>
                     <li><a><span className="flaticon-analysis"></span> CV Manager</a></li>
-                    <li><a><span className="flaticon-favorites"></span> Favourite Jobs</a></li>
-                    <li><a ><span className="flaticon-chat"></span> Messages</a></li>
-                    <li><a><span className="flaticon-rating"></span> Reviews</a></li>
-                    <li><a ><span className="flaticon-alarm"></span> Job Alerts</a></li>
-                    <li><a ><span className="flaticon-locked"></span> Change Password</a></li>
-                    <li><a ><span className="flaticon-logout"></span> Logout</a></li>
-                    <li><a ><span className="flaticon-rubbish-bin"></span> Delete Profile</a></li>
+                    <li><a><span className="flaticon-favorites"></span> Favori İlanlar</a></li>
+                    <li><a ><span className="flaticon-locked"></span> Parola değiştir</a></li>
+                    <li><a ><span className="flaticon-logout"></span> Çıkış Yap</a></li>
+                    <li><a ><span className="flaticon-rubbish-bin"></span> Profili Sil</a></li>
                 </ul>
             </div>
         </div>
